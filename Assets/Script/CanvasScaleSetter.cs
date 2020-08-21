8ea0a1ca71c703d3f981068c88249f18ae5149b2
starcjm <starcjm@naver.com>
2020-08-21 19:09:09 +0900

    해상도 이슈 수정

---
+++ Assets/Script/CanvasScaleSetter.cs
@@ -0,0 +1,18 @@
+﻿/***************************************************************************************
+ * HANDLES THE CANVAS SIZE BASED ON DEVICE RESOLUTION.
+ ***************************************************************************************/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class CanvasScaleSetter : MonoBehaviour
+{
+    void Start()
+    {
+        if (GameManager.Instance.screenAspect > 1.86F)
+        {
+            GetComponent<CanvasScaler>().matchWidthOrHeight = 0;
+        }
+    }
+}
---
+++ Assets/Script/Const.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const
+{
+    public const int screenWidth = 720;
+    public const int screenHeight = 1280;
+    public const int GAME_FRAME_RATE = 60;
+}
